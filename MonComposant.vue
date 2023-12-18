bef69a8d9520df13ba6411b7bac67225879e99ce
adelfortin <contact@adelfortin.com>
2023-12-18 15:42:21 +0100

    ajout des modifs

---
+++ MonComposant.vue
@@ -0,0 +1,18 @@
+<<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+    
+    <MonComposant>
+
+</template>
+
+data : () => 
+  {    return 
+    {        tamagotchi : 
+      {            
+        satiety : 100,            
+        stamina : 100,            
+        fun : 100        
+      }    
+    }
+  }
